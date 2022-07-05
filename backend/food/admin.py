803442af3b2004eb5a28179accb18e7494423e77
Игорь Бутаков <bikrtw@gmail.com>
2022-07-05 08:54:45 +0300

    register models for admin

--- backend/food/admin.py
+++ backend/food/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+admin.site.register(models.Tag)
+admin.site.register(models.Recipe)
+admin.site.register(models.Ingredient)
+admin.site.register(models.FavoriteRecipe)
+admin.site.register(models.RecipeTag)
+admin.site.register(models.ShoppingCart)
+admin.site.register(models.Subscription)
+

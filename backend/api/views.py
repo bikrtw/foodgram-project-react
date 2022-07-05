775faff92321eb0a639c760fb37ec606c8766be8
Игорь Бутаков <bikrtw@gmail.com>
2022-07-05 08:59:38 +0300

    add initial views configuration

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
 
-# Create your views here.
+from . import serializers
+from ..food import models
+
+
+class UserViewSet:
+    # TODO
+    pass
+
+
+class TagViewSet(mixins.RetrieveModelMixin,
+                 mixins.ListModelMixin,
+                 viewsets.GenericViewSet):
+    serializer_class = serializers.TagSerializer
+    queryset = models.Tag.objects.all()
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    queryset = models.Recipe.objects.all()
+    serializer_class = serializers.RecipeSerializer
+
+
+class IngredientViewSet(mixins.RetrieveModelMixin,
+                        mixins.ListModelMixin,
+                        viewsets.GenericViewSet):
+    serializer_class = serializers.IngredientSerializer
+    queryset = models.Ingredient.objects.all()

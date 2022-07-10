06fd6295987bb7ba13db26cbe16399db5b4d13bc
Игорь Бутаков <bikrtw@gmail.com>
2022-07-10 13:18:22 +0300

    configure basic urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,13 +1,18 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from . import views
 
 router = DefaultRouter()
+router.register(r'users', views.UserViewSet)
 router.register(r'tags', views.TagViewSet)
+router.register(r'recipes', views.RecipeViewSet)
 
-urlpatterns = [
+urlpatterns = router.urls
 
+urlpatterns += [
+    path('', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
 ]
 
-urlpatterns += router.urls
+

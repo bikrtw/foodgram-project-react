4f20f8ae4e85ddf07750a992bcfa55a2d48237c3
Игорь Бутаков <bikrtw@gmail.com>
2022-07-16 10:54:33 +0300

    add AuthorOrReadOnly permission

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -1,8 +1,19 @@
-from rest_framework.permissions import BasePermission
+from rest_framework import permissions
 
 
-class Forbidden(BasePermission):
+class Forbidden(permissions.BasePermission):
     """Permission to disable some basic djoser views."""
 
     def has_permission(self, request, view):
         return False
+
+
+class AuthorOrReadOnly(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated)
+
+    def has_object_permission(self, request, view, obj):
+        return (request.method in permissions.SAFE_METHODS
+                or obj.author == request.user)
